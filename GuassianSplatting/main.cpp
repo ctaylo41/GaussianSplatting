7b3cb228ec685296082ec4d7d4b0840a41e24954
ctaylo41 <117548804+ctaylo41@users.noreply.github.com>
2025-12-24 16:35:36 -0500

    Initial Commit

---
+++ GuassianSplatting/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GuassianSplatting
+//
+//  Created by Colin Taylor Taylor on 2025-12-24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
